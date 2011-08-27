f03f88fb07e139db791f3864d2e911ac7a9f22ec
Peter Graham <peterghm@gmail.com>
2011-08-27 13:52:58 -0400

    add jquery,mongo,excanvas, start script

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var ejs = require('ejs')
 , nko = require('nko')('6coPfJMuWg55y54B')
-, express = require('express');
+, express = require('express')
+, mongo = require('mongoose');
 
 app = express.createServer();
 app.set('view engine', 'ejs');
 app.configure(function(){
   app.use(express.static(__dirname + '/static'));
 });
+
+if(process.env.MONGOHQ_URI) {
+  console.log("connecting to mongohq...");
+  mongo.connect(process.env.MONGOHQ_URI);
+}
+else {
+  console.log("connecting to local mongo");
+  mongo.connect('mongodb://localhost/nko-shoujo');
+}
 
 app.get('/', function (req, res) {
   res.render('index')
