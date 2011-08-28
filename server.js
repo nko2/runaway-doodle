e123d9aa8a93997c5967364d62738bd4de53feae
Peter Graham <peterghm@gmail.com>
2011-08-28 12:47:56 -0400

    insert doodle into db, list and show views/controllers

--- server.js
+++ server.js
@@ -5,21 +5,33 @@ var ejs = require('ejs')
 , _ = require('underscore')
 , mongo = require('mongoose');
 
-app = express.createServer();
-app.set('view engine', 'ejs');
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/static'));
-});
 
+Schema = mongo.Schema;
 if(process.env.MONGOHQ_URI) {
   console.log("connecting to mongohq...");
   mongo.connect(process.env.MONGOHQ_URI);
 }
 else {
   console.log("connecting to local mongo");
-  mongo.connect('mongodb://localhost/nko-shoujo');
+  mongo.connect('mongodb://localhost:27017/nko-shoujo');
 }
+
+var ImageSchema = new Schema({
+  data: {type:String, match:/^data:image\//}
+  , phrase: String
+  , name: String
+  , created_at: {type:Date, default: Date.now}
+});
+mongo.model('Image', ImageSchema);
+var Image = mongo.model('Image');
+
+
+app = express.createServer();
+app.set('view engine', 'ejs');
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/static'));
+});
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -39,8 +51,26 @@ app.get('/random-phrase', function(req, res){
 });
 
 app.post('/insert-image', function(req, res){
-  console.log(req.body.image);
+  new Image({data: req.body.image, phrase: req.body.phrase, name: req.body.name}).save();
   res.send("ok");
+});
+
+app.get('/latest', function(req, res){
+  Image.find({}, [], {sort: [['created_at','descending']], limit: 20}, function(err, images){
+    if(err){res.send("bad");}
+    else{
+      res.render('list', {images: images});
+    }
+  });
+});
+
+app.get('/:id', function(req,res){
+  Image.findById(req.params.id, function(err, image){
+    if(err){res.send("not found");}
+    else{
+      res.render("show", {image: image});
+    }
+  });
 });
 
 app.listen(parseInt(process.env.PORT) || 3333);
